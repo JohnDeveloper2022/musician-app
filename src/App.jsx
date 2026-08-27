0cf65d2a7a02b1870d0eb49104b2f00d63d91a09
Johnny <jgamb.rod@gmail.com>
2026-08-27 15:56:02 -0300

    feat: connect home to intention ritual

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import IntentionRitual from './pages/IntentionRitual'
 
 function App() {
-  return <Home />
+  const [currentScreen, setCurrentScreen] = useState('home')
+  
+  return (
+    <>
+      {currentScreen === 'home' && (
+        <Home onStartSession={() => setCurrentScreen('intention')} />
+      )}
+
+      {currentScreen === 'intention' && (
+        <IntentionRitual />
+      )}
+    </>
+  )
 }
 
 export default App
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import './Button.css'
 
-function Button({ children, variant = 'primary', disabled = false, icon }) {
+function Button({ children, variant = 'primary', disabled = false, icon, onClick }) {
     return (
         <button 
             type='button'
             className='button'
             data-variant={variant}
             disabled={disabled}
+            onClick={onClick}
         >   
             {icon && (
                 <span className='material-symbols-outlined'>
